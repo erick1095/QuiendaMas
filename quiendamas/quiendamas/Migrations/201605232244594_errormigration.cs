e41281288b8bc09108a98657fad2ad670809c5d9
Rangel <erick_1095@hotmail.com>
2016-05-23 15:45:49 -0700

    migracion hecha

---
+++ quiendamas/quiendamas/Migrations/201605232244594_errormigration.cs
@@ -0,0 +1,16 @@
+namespace quiendamas.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class errormigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
